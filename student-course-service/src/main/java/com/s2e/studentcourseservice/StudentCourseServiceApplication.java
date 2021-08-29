7cd2dffa166fdf48114d94e640c9cbd95ffbf477
Chanel Boris <chanelboris@gmail.com>
2021-08-29 14:29:21 +0200

    Generated course service

--- student-course-service/src/main/java/com/s2e/studentcourseservice/StudentCourseServiceApplication.java
+++ student-course-service/src/main/java/com/s2e/studentcourseservice/StudentCourseServiceApplication.java
@@ -2,8 +2,10 @@ package com.s2e.studentcourseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentCourseServiceApplication {
 
 	public static void main(String[] args) {
