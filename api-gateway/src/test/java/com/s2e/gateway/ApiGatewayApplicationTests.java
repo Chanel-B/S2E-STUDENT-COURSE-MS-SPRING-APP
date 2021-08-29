b27ff231506ff8bc9ec20fb61f56be8fdb03796c
Chanel Boris <chanelboris@gmail.com>
2021-08-29 14:54:48 +0200

    Generated API-SERVICE

---
+++ api-gateway/src/test/java/com/s2e/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2e.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
