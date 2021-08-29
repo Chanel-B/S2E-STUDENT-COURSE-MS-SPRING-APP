c80d66685b299073597100cdbb1e5c75db10f54e
Chanel Boris <chanelboris@gmail.com>
2021-08-29 13:40:21 +0200

    Completely registered Student-service in Service-Registry

--- student-service/src/main/java/com/s2e/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/com/s2e/studentservice/StudentServiceApplication.java
@@ -2,13 +2,15 @@ package com.s2e.studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
-		System.out.println("Student Servie Running...");
+		System.out.println("Student Service Running...");
 	}
 
 }
